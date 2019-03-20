e0fa5c1ca427491aa5c5b584dcb1c1110bc176fe
lucasEngdComp <48529983+lucasEngdComp@users.noreply.github.com>
2019-03-19 21:22:36 -0300

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -41,3 +41,41 @@ public class Banco {
         return false; // falta implementar com Test-driven development
     }
 }
+
+// TESTE - ------------------------
+
+import java.util.Scanner;
+ 
+public class String9 {
+ 
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+ 
+    String s;
+    char c;
+    int i, n, ctalfa = 0, ctnum = 0;
+ 
+    System.out.printf("Informe uma String:\n");
+    s = ler.nextLine();
+ 
+    System.out.println();
+ 
+    n = s.length(); // tamanho da string (qtde de caracteres)
+    for (i=0; i<n; i++) {
+      c = s.charAt(i); // i-ésimo caractere da string 's'
+// verificando se é um caractere alfabético
+      if (((c >= 'A') && (c <= 'Z')) ||
+          ((c >= 'a') && (c <= 'z'))) {
+         ctalfa = ctalfa + 1;
+         System.out.printf("%c ---> %da. letra\n", c, ctalfa);
+      }
+// verificando se é um caractere numérico
+      else if ((c >= '0') && (c <= '9')) {
+              ctnum = ctnum + 1;
+              System.out.printf("%c ---> %do. número\n", c, ctnum);
+           }
+           else System.out.printf("%c\n", c);
+    }
+  }
+ 
+}
